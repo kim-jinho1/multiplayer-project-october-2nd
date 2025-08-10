4ff19eaea9f035cd6ff90c86be18ab9af4e8e135
kim-jinho1 <russiakim2008@gmail.com>
2025-08-10 23:02:01 +0900

    feat : bootstrap.cs

---
+++ Assets/01.Code/GameInitialization/Bootstrap.cs
@@ -0,0 +1,25 @@
+using Code.CoreGameLogic;
+using UnityEngine;
+using Code.Global;
+
+namespace GameInitialization
+{
+    /// <summary>
+    /// 게임 시작 시 필요한 모든 초기화 및 의존성 등록을 수행하는 스크립트
+    /// </summary>
+    public class Bootstrap : MonoBehaviour
+    {
+        private void Awake()
+        {
+            DependencyContainer.InitializeGameDependencies();
+            Debug.Log("DependencyContainer 초기화 완료.");
+            
+            if (FindObjectOfType<GameManager>() == null)
+            {
+                GameObject gameManagerGO = new GameObject("GameManager");
+                gameManagerGO.AddComponent<GameManager>();
+                Debug.Log("GameManager GameObject 생성 및 컴포넌트 추가 완료.");
+            }
+        }
+    }
+}
