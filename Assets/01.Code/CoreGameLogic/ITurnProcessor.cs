fd20d00fe72ba3c7056c3ae8c54e715f715637f8
kim-jinho1 <russiakim2008@gmail.com>
2025-08-10 16:09:23 +0900

    feat : ITurnProcessor

--- Assets/01.Code/CoreGameLogic/ITurnProcessor.cs
+++ Assets/01.Code/CoreGameLogic/ITurnProcessor.cs
@@ -1,7 +1,13 @@
 namespace Code.CoreGameLogic
 {
+    /// <summary>
+    /// 한 턴의 모든 로직(예: 국정 턴, 기물 턴)을 실행하는 인터페이스입니다.
+    /// </summary>
     public interface ITurnProcessor
-    { 
-        void ExecuteTurn();
+    {
+        /// <summary>
+        /// 한 턴의 시작부터 끝까지 필요한 모든 처리를 순차적으로 실행합니다.
+        /// </summary>
+        void ProcessTurn();
     }
 }
