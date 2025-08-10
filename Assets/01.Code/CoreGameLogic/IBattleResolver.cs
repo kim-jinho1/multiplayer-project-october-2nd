08484f6eae2ea466e853cea062ebc42f14833c09
kim-jinho1 <russiakim2008@gmail.com>
2025-08-10 16:08:57 +0900

    feat : IBattleResolver

---
+++ Assets/01.Code/CoreGameLogic/IBattleResolver.cs
@@ -0,0 +1,13 @@
+namespace Code.CoreGameLogic
+{
+    /// <summary>
+    /// 기물 간의 전투를 처리하는 인터페이스입니다.
+    /// </summary>
+    public interface IBattleResolver
+    {
+        /// <summary>
+        /// 공격자와 방어자의 전투를 처리하고, 피해량과 승패 결과를 적용합니다.
+        /// </summary>
+        void ResolveBattle(Piece attacker, Piece defender);
+    }
+}
