5b1c3884c419892f804a541f8ad7f0030bdf29c7
kim-jinho1 <russiakim2008@gmail.com>
2025-08-10 16:12:00 +0900

    feat : ITechnologyManager

--- Assets/01.Code/StrategicSystem/ITechnologyManager.cs
+++ Assets/01.Code/StrategicSystem/ITechnologyManager.cs
@@ -1,7 +1,23 @@
-namespace _01.Code.StrategicSystem
+namespace Code.StrategicSystem
 {
+    /// <summary>
+    /// 기술 연구와 관련된 기능을 추상화하는 인터페이스입니다.
+    /// </summary>
     public interface ITechnologyManager
     {
-        
+        /// <summary>
+        /// 기술 연구를 진행합니다.
+        /// </summary>
+        void ResearchTechnology();
+
+        /// <summary>
+        /// 현재 기술 연구가 가능한지 확인합니다.
+        /// </summary>
+        bool CanResearch();
+
+        /// <summary>
+        /// 현재 기술 수준에 따른 효과를 적용합니다.
+        /// </summary>
+        void ApplyTechnologyEffects();
     }
 }
