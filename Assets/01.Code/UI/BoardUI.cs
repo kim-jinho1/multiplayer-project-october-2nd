5160a7ba340005283506aba7518bca3d9ad25f8b
kim-jinho1 <russiakim2008@gmail.com>
2025-08-20 22:41:41 +0900

    remove : scripts

--- Assets/01.Code/UI/BoardUI.cs
+++ Assets/01.Code/UI/BoardUI.cs
@@ -5,11 +5,11 @@ namespace Code.UI
 {
     public class BoardUI : MonoBehaviour
     {
-        public void MovePieceView(PieceView pieceView, Vector2 to)
-        {
-            pieceView.transform.position = new Vector3(to.x, 0, to.y);
-            pieceView.CurrentPosition = to;
-            Debug.Log($"{pieceView.LogicalPiece.PieceName}가 {to} 위치로 이동했습니다.");
-        }
+        // public void MovePieceView(PieceView pieceView, Vector2 to)
+        // {
+        //     pieceView.transform.position = new Vector3(to.x, 0, to.y);
+        //     pieceView.CurrentPosition = to;
+        //     Debug.Log($"{pieceView.LogicalPiece.PieceName}가 {to} 위치로 이동했습니다.");
+        // }
     }
 }
